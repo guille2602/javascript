a84468c551272ea9aee0e26489bc37c6b96877a4
Guillermo Seclen Alfaro <guille.2602@gmail.com>
2022-08-09 10:47:50 -0300

    Agregados comentarios e informo vuelto

--- prueba.js
+++ prueba.js
@@ -1,15 +1,20 @@
 function convertirMoneda(divisa) {
     let importe = prompt('Ingrese cantidad de pesos a convertir');
     let valormoneda = 0;
+
+    // Evaluo el tipo de cambio, aquí se debe actualizar el valor de la divisa extranjera.
     if (divisa == 'euros') {
         valormoneda = 315;
     } else {
         valormoneda = 287;
     }
 
+    //Calculo la cantidad de moneda extranjera
     let cantmoneda = parseInt(importe) / valormoneda;
     cantmoneda = parseInt(cantmoneda);
+    let resto = parseInt(importe) - cantmoneda * valormoneda;
 
+    // Muestro el resultado y el vuelto
     alert('Usted puede comprar ' + cantmoneda + ' ' + divisa);
-    console.log('Usted puede comprar ' + cantmoneda + ' ' + divisa);
+    alert('Le sobran ' + resto + ' pesos');
 }
